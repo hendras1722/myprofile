2fdfd608b186aac0e6b72b489fe97adc815e4c22
Muh Syahendra Anindyantoro <syahendra@privyid.tech>
2023-05-27 02:59:44 +0700

    release profile

--- src/components/Me.tsx
+++ src/components/Me.tsx
@@ -7,15 +7,39 @@ function Me() {
   const [stateGithub, setStateGithub] = useState<Github>([]);
   // const [stateLinkedin, setStateLinkedin] = useState<string>("");
   const [stateWhatsapp, setStateWhatsapp] = useState<string>("");
+  const [open, setOpen] = useState<boolean>(false);
+
+  function detectMob() {
+    const toMatch = [
+      /Android/i,
+      /webOS/i,
+      /iPhone/i,
+      /iPad/i,
+      /iPod/i,
+      /BlackBerry/i,
+      /Windows Phone/i,
+    ];
+
+    return toMatch.some((toMatchItem) => {
+      return navigator.userAgent.match(toMatchItem);
+    });
+  }
 
   const handleClick = async (e): Promise<void> => {
     switch (e) {
       case "github":
+        setOpen(false);
         try {
           const res = await fetch(
             "https://api.github.com/users/hendras1722/repos"
           );
           const data = (await res.json()) as Github;
+          setTimeout(() => {
+            window.scroll({
+              behavior: "smooth",
+              top: 500,
+            });
+          }, 300);
           setStateGithub(data);
         } catch (error) {
           console.error(error.message);
@@ -23,12 +47,35 @@ function Me() {
         break;
       case "linkedin":
         setStateGithub([]);
-        // setStateLinkedin("https://www.linkedin.com/in/muhsyahendraa/");
+        setOpen(false);
+        window.open(`https://www.linkedin.com/in/muhsyahendraa/`, "_blank");
+        break;
+      case "whatsapp":
+        window.scroll({
+          behavior: "smooth",
+          top: 30,
+        });
+        setStateGithub([]);
+        setOpen(true);
         break;
 
       default:
         break;
     }
+  };
+
+  const handleWhatsapp = () => {
+    console.log(detectMob(), "inim");
+    if (detectMob()) {
+      return window.open(
+        `whatsapp://send?text=${encodeURIComponent(stateWhatsapp)}`,
+        "_blank"
+      );
+    }
+    window.open(
+      `https://wa.me/089663604258?text=${encodeURIComponent(stateWhatsapp)}`,
+      "_blank"
+    );
   };
 
   function Feature({ ...rest }) {
@@ -42,7 +89,9 @@ function Me() {
         {...rest}
       >
         <Heading fontSize="4xl">{title}</Heading>
-        <Text marginTop={"20px"}>{desc}</Text>
+        <Text marginTop={"20px"} textAlign={"justify"}>
+          {desc}
+        </Text>
         {rest["children"] && (
           <>
             <div>{rest["children"]}</div>
@@ -51,9 +100,10 @@ function Me() {
       </Box>
     );
   }
+
   return (
     <>
-      <div style={{ textAlign: "left" }} className="lg:px-28">
+      <div style={{ textAlign: "left" }} className="lg:px-28 pb-9">
         <div>
           <HStack spacing={8} alignItems={"start"}>
             <div className="grid md:grid-cols-2   sm:grid-cols-1 grid-rows-1 gap-1 p-5">
@@ -74,13 +124,13 @@ function Me() {
                     }}
                     className="m-4"
                     title="Muh Syahendra Anindyantoro"
-                    desc="Hi, Saya Muh Syahendra Anindyantoro biasa dipanggil hendra. Profesi sebagai Frontend Developer atau Frontend Engineer. Kehidupan saya sehari hari sebagai Frontend Developer selalu didepan laptop. Mencovert design menjadi sebuah HTML dan mengintegrasi endpoint yang dibutuhkan."
+                    desc="Hi, I'm Muh Syahendra Anindyantoro usually called hendra. I work as a Frontend Developer or Frontend Engineer. My daily life as a Frontend Developer is always in front of a laptop. Covering the design into HTML and integrating the required endpoints."
                   />
                 </div>
-                <div className="mt-8 px-4">
-                  <Divider marginTop={10} />
+                <div className="mt-5 px-4">
+                  <Divider marginTop={5} />
                   <div className="mt-3 text-lg font-medium">
-                    Alamat:{" "}
+                    Address:{" "}
                     <span className="text-sm font-medium">
                       Jl Beo 1 Perum Barito 1 blok A5 Rt 2/X Gonilan Kartasura
                     </span>
@@ -102,41 +152,16 @@ function Me() {
                       </a>
                     </span>
                   </div>
-                  <div className="mt-3">
-                    <div className="text-lg font-medium">Me:</div>
-                    <span className="mt-3">
-                      <div className="flex justify-around md:justify-start mt-5 ">
-                        <div className="md:mr-5 mr-3">
-                          <div>
-                            <button
-                              onClick={() => handleClick("github")}
-                              className="bg-transparent hover:bg-black text-black font-semibold hover:text-white py-2 px-4 border border-black hover:border-transparent rounded"
-                            >
-                              Github
-                            </button>
-                          </div>
-                        </div>
-                        <div className="md:mr-5 mr-3">
-                          <div>
-                            <button
-                              onClick={() => handleClick("linkedin")}
-                              className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-                            >
-                              Linkedin
-                            </button>
-                          </div>
-                        </div>
-                        <div className="md:mr-5">
-                          <div>
-                            <button
-                              onClick={() => handleClick("whatsapp")}
-                              className="bg-transparent hover:bg-green-500 text-green-500 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded"
-                            >
-                              Whatsapp
-                            </button>
-                          </div>
-                        </div>
-                      </div>
+                  <div className="mt-3 text-lg font-medium">
+                    Skill:{" "}
+                    <span className="text-sm font-medium">
+                      ReactJS | NextJS | Nuxt | Vue
+                    </span>
+                  </div>
+                  <div className="mt-3 text-lg font-medium">
+                    CV:{" "}
+                    <span className="text-sm font-medium">
+                      <a href="">Download</a>
                     </span>
                   </div>
                 </div>
@@ -145,73 +170,111 @@ function Me() {
           </HStack>
           <div className="px-4">
             <hr />
-            {/* <iframe src="https://www.w3schools.com" title="linkedin"></iframe> */}
-            Whatsapp
-            <div>
-              <form class="bg-white px-8 pt-6 pb-8 mb-4">
-                <div class="mb-4">
-                  <label
-                    class="block text-gray-700 text-sm font-bold mb-2"
-                    for="username"
-                  >
-                    text
-                  </label>
-                  {/* whatsapp://send?abid=username&text=Hello%2C%20World! */}
-                  {stateWhatsapp}
-                  <textarea
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    type="text"
-                    placeholder="Username"
-                    onChange={(e) => setStateWhatsapp(e.target.value)}
-                  />
-                </div>
-                <div class="mb-6">
-                  <label
-                    class="block text-gray-700 text-sm font-bold mb-2"
-                    for="username"
-                  >
-                    Username
-                  </label>
-                  <input
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="username"
-                    type="text"
-                    placeholder="Username"
-                  />
-                </div>
-                <div class="flex items-center justify-between">
-                  <button
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    type="button"
-                  >
-                    Sign In
-                  </button>
-                </div>
-              </form>
-            </div>
-            {stateGithub.length > 0 &&
-              stateGithub.map((item, index) => (
-                <div className="mt-5 p-5 border-2 rounded-md" key={index}>
-                  <div>{item.name}</div>
-                  <div>{item.description}</div>
-                  <div
-                    className={`${
-                      item.language &&
-                      item.language.toLowerCase() === "javascript"
-                        ? "bg-yellow-200  w-fit py-1 px-2 rounded-full bg-yellow-200 text-sm"
-                        : item.language && item.language.toLowerCase() === "vue"
-                        ? "bg-green-200 w-fit py-1 px-2 rounded-full bg-yellow-200 text-sm"
-                        : item.language &&
-                          item.language.toLowerCase() === "typescript"
-                        ? "bg-blue-200 w-fit py-1 px-2 rounded-full bg-yellow-200 text-sm"
-                        : !item.language
-                        ? ""
-                        : "bg-gray-200 w-fit py-1 px-2 rounded-full bg-yellow-200 text-sm"
-                    }`}
-                  >
-                    {item.language}
+            <div className="mt-3">
+              <span className="mt-3">
+                <div className="flex justify-around md:justify-center mt-5 ">
+                  <div className="md:mr-5 mr-3">
+                    <div>
+                      <button
+                        onClick={() => handleClick("github")}
+                        className="bg-transparent hover:bg-black text-black font-semibold hover:text-white py-2 px-4 border border-black hover:border-transparent rounded"
+                      >
+                        Github
+                      </button>
+                    </div>
+                  </div>
+                  <div className="md:mr-5 mr-3">
+                    <div>
+                      <button
+                        onClick={() => handleClick("linkedin")}
+                        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                      >
+                        Linkedin
+                      </button>
+                    </div>
+                  </div>
+                  <div className="md:mr-5">
+                    <div>
+                      <button
+                        onClick={() => handleClick("whatsapp")}
+                        className="bg-transparent hover:bg-green-500 text-green-500 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded"
+                      >
+                        Whatsapp
+                      </button>
+                    </div>
                   </div>
                 </div>
+              </span>
+            </div>
+            {/* <iframe src="https://www.w3schools.com" title="linkedin"></iframe> */}
+            {open && (
+              <div className="mt-8">
+                <div
+                  className="flex justify-center font-bold text-2xl"
+                  style={{ color: "#23D366" }}
+                >
+                  Whatsapp
+                </div>
+                <form className="bg-white px-8 pt-6 pb-8 mb-4">
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="username"
+                    >
+                      Pesan:
+                    </label>
+                    <textarea
+                      className="shadow h-24 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      type="text"
+                      placeholder="Username"
+                      onChange={(e) => setStateWhatsapp(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="flex items-center justify-center">
+                    <button
+                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                      type="button"
+                      onClick={handleWhatsapp}
+                      style={{ background: "#23D366" }}
+                    >
+                      Open Whatsapp
+                    </button>
+                  </div>
+                </form>
+              </div>
+            )}
+            {stateGithub.length > 0 &&
+              stateGithub.map((item, index) => (
+                <a href={item.svn_url} key={index}>
+                  <div className="mt-5 p-5 border-2 rounded-md">
+                    <div className="mt-3">
+                      <span className="font-semibold">Name :</span> {item.name}
+                    </div>
+                    <div className="mt-3">
+                      <span className="font-semibold">Description :</span>{" "}
+                      {item.description}
+                    </div>
+                    <div
+                      className={`${
+                        item.language &&
+                        item.language.toLowerCase().includes("javascript")
+                          ? "bg-yellow-200  w-fit py-1 px-2 rounded-full text-sm mt-3"
+                          : item.language &&
+                            item.language.toLowerCase().includes("vue")
+                          ? "bg-green-200 w-fit py-1 px-2 rounded-full text-sm mt-3"
+                          : item.language &&
+                            item.language.toLowerCase().includes("typescript")
+                          ? "bg-blue-200 w-fit py-1 px-2 rounded-full text-sm mt-3"
+                          : !item.language
+                          ? ""
+                          : "bg-gray-200 w-fit py-1 px-2 rounded-full text-sm mt-3"
+                      }`}
+                    >
+                      {item.language}
+                    </div>
+                  </div>
+                </a>
               ))}
           </div>
         </div>
