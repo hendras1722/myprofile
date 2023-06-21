4c5720d851e1847eb7513de88cbaed529ac394e1
Muh Syahendra Anindyantoro <syahendra@privyid.tech>
2023-06-21 22:15:20 +0700

    add source name and age

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -42,6 +42,8 @@ function App() {
   };
 
   useEffect(() => {
+    const typing_text = document.getElementById(`typing_text0`);
+    if (typing_text) typing_text.focus();
     sessionStorage.removeItem("poor");
   }, []);
 
@@ -49,7 +51,7 @@ function App() {
     window.location.reload();
   };
 
-  const typeText = () => {
+  const typeText = (e: string) => {
     const typed = new Typed({
       callback: (text) => {
         if (el.current) {
@@ -58,49 +60,88 @@ function App() {
       },
     });
     const type = async () => {
-      const line1 = "npm install MSA\n";
-      const line2 = "Installing components...\n";
-      const line3 = "Fetching from source...\n";
-      const line4 = "Success 200 🤪\n";
-      const line5 = `Failed 403 😭\n`;
-
+      let line1 = "";
+      let line2 = "";
+      let line3 = "";
+      let line4 = "";
+      let line5 = "";
+      let randomValue = undefined;
       const a = [true, false];
-      const randomValue = a[Math.floor(a.length * Math.random())];
-      console.log(randomValue);
-      typed.type(line1, { errorMultiplier: 0 });
-      typed.wait(3000);
-      typed.type(line2, { errorMultiplier: 0 });
-      typed.wait(3000);
-      typed.type(line3, { errorMultiplier: 0 });
-      typed.wait(3000);
+      if (e === "npm") {
+        randomValue = a[Math.floor(a.length * Math.random())];
+        line1 = "npm install MSA\n";
+        line2 = "Installing components...\n";
+        line3 = "Fetching from source...\n";
+        line4 = "Success 200 🤪\n";
+        line5 = `Failed 403 😭\n`;
+      }
+      if (e === "name") {
+        line1 = "My Name is Muh Syahendra A";
+        line2 = "";
+        line3 = "";
+        line4 = "";
+        line5 = ``;
+      }
+      const birtday = new Date("1996-12-22");
+      const today = new Date();
+
+      const ageMilisecond = Number(today) - Number(birtday);
+      const ageYears = Math.floor(
+        ageMilisecond / (1000 * 60 * 60 * 24 * 365.25)
+      );
+
+      if (e === "age") {
+        line1 = ageYears + " " + "years old";
+        line2 = "";
+        line3 = "";
+        line4 = "";
+        line5 = ``;
+      }
+
+      if (line1) {
+        typed.type(line1, { errorMultiplier: 0 });
+        typed.wait(1500);
+      }
+      if (line2) {
+        typed.type(line2, { errorMultiplier: 0 });
+        typed.wait(1500);
+      }
+      if (line3) {
+        typed.type(line3, { errorMultiplier: 0 });
+        typed.wait(1500);
+      }
       if (randomValue) {
-        typed.type(line4, { errorMultiplier: 0, className: "successTyped" });
-        typed.wait(3000);
-        typed.type("====================\n", { errorMultiplier: 0 });
-        typed.wait(1000);
-        typed.type("MSA Begin\n", {
-          errorMultiplier: 0,
-          className: "fontWeight",
-        });
-        typed.wait(1000);
-        typed.type("====================\n", { errorMultiplier: 0 });
-        typed.wait(1000);
+        if (line4) {
+          typed.type(line4, { errorMultiplier: 0, className: "successTyped" });
+          typed.wait(1500);
+          typed.type("====================\n", { errorMultiplier: 0 });
+          typed.wait(1000);
+          typed.type("MSA Begin\n", {
+            errorMultiplier: 0,
+            className: "fontWeight",
+          });
+          typed.wait(1000);
+          typed.type("====================\n", { errorMultiplier: 0 });
+          typed.wait(1000);
+        }
       } else {
-        typed.type(line5, {
-          errorMultiplier: 0,
-          noSpecialCharErrors: true,
-          className: "errorTyped",
-        });
-        typed.wait(3000);
-        typed.type("====================\n", { errorMultiplier: 0 });
-        typed.wait(1000);
-        typed.type("Oh no, please try again...\n", {
-          errorMultiplier: 0,
-          className: "fontWeight",
-        });
-        typed.wait(1000);
-        typed.type("====================\n", { errorMultiplier: 0 });
-        typed.wait(1000);
+        if (line5) {
+          typed.type(line5, {
+            errorMultiplier: 0,
+            noSpecialCharErrors: true,
+            className: "errorTyped",
+          });
+          typed.wait(3000);
+          typed.type("====================\n", { errorMultiplier: 0 });
+          typed.wait(1000);
+          typed.type("Oh no, please try again...\n", {
+            errorMultiplier: 0,
+            className: "fontWeight",
+          });
+          typed.wait(1000);
+          typed.type("====================\n", { errorMultiplier: 0 });
+          typed.wait(1000);
+        }
       }
       await typed.run();
       setDataTerminal((prev) => [
@@ -109,6 +150,8 @@ function App() {
           html: "input",
         },
       ]);
+      // const typing_text = document.getElementById(`typing_text${e + 1}`);
+      // typing_text?.focus();
       setCodeStatus(randomValue);
 
       // typed.reset();
@@ -121,6 +164,7 @@ function App() {
     console.log(e, "inie");
     const typing_text = document.getElementById(`typing_text${props.index}`);
     value = typing_text?.innerText;
+    console.log(value, "inivalue");
     const charCode = e.keyCode || e.which;
     if (!typing_text) return;
     if (charCode === 13 || e.key === "Enter") {
@@ -128,24 +172,48 @@ function App() {
       typing_text.setAttribute("contentEditable", "false");
 
       if (value?.includes("npm")) {
-        if (value.includes("npm i msa") || value.includes("npm install msa")) {
+        if (
+          value.includes("npm i") ||
+          value.includes("npm install") ||
+          value.includes("npm i msa") ||
+          value.includes("npm install msa")
+        ) {
           setDataTerminal([
             ...dataTerminal,
             {
               html: "text",
             },
           ]);
-          typeText();
+          typeText("npm");
         }
         return;
-      } else {
+      }
+      if (value?.includes("name")) {
         setDataTerminal([
           ...dataTerminal,
           {
-            html: "input",
+            html: "text",
           },
         ]);
+        typeText("name");
+        return;
       }
+      if (value?.includes("umur") || value?.includes("age")) {
+        setDataTerminal([
+          ...dataTerminal,
+          {
+            html: "text",
+          },
+        ]);
+        typeText("age");
+        return;
+      }
+      setDataTerminal([
+        ...dataTerminal,
+        {
+          html: "input",
+        },
+      ]);
 
       // return;
     }
@@ -204,65 +272,14 @@ function App() {
           </div>
         </div>
         <div
-          className="bg-black h-96 p-2 items-center h-9/6"
+          className="bg-black h-96 p-2 items-center h-9/6 overflow-auto py-3"
           id="container_typing"
         >
-          {dataTerminal.map(
-            (item, index) => (
-              <div key={index}>{HtmlTerminal({ item, index })}</div>
-            )
-            // <div key={index}>
-            //   <HtmlTerminal item={item} index={index} />
-            // </div>
-          )}
-          {/* <div
-            id="typing_text"
-            contentEditable
-            className="text-white focus:outline-none"
-            onKeyDown={handleSubmit}
-          ></div> */}
-          {/* <div
-            style={{
-              whiteSpace: "pre-line",
-            }}
-            className="text-white"
-          >
-            $ <span ref={el} /> <span>|</span>
-          </div> */}
+          {dataTerminal.map((item, index) => (
+            <div key={index}>{HtmlTerminal({ item, index })}</div>
+          ))}
         </div>
 
-        {/* <div style={{ position: "relative" }}>
-          <div
-            style={{
-              position: "absolute",
-              color: "white",
-              fontWeight: "bold",
-              right: 10,
-              top: 8,
-
-              borderRadius: "50%",
-              width: 10,
-              height: 10,
-              background: "red",
-            }}
-          ></div>
-        </div> */}
-        {/* <div
-          style={{
-            background: "black",
-            padding: 10,
-            height: 300,
-            borderTop: "25px solid blue",
-            borderRadius: 8,
-            color: "white",
-            textAlign: "left",
-            fontSize: 22,
-            whiteSpace: "pre-line",
-            overflow: "auto",
-          }}
-        >
-          $ <span ref={el} /> <span>|</span>
-        </div> */}
         <Modal onClose={onClose} size={"full"} isOpen={isOpen}>
           <ModalContent className="modalContent">
             <ModalBody className="modalBodyCustom">
