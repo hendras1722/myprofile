58762c0929dbb9783c90db496255db9ad64745b2
Muh Syahendra Anindyantoro <syahendra@privyid.tech>
2023-06-22 15:57:21 +0700

    fixing clear terminal code

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -46,7 +46,232 @@ function App() {
   useEffect(() => {
     const typing_text = document.getElementById(`typing_text0`);
     if (typing_text) typing_text.focus();
-    typeText("mount");
+    const container_typing = document.getElementById("container_typing");
+    const typeTexts = (e: string, value?: string) => {
+      const typed = new Typed({
+        callback: (text) => {
+          if (el.current) {
+            el.current.innerHTML = text;
+          }
+        },
+      });
+      const type = async () => {
+        let line1 = "";
+        let line2 = "";
+        let line3 = "";
+        let line4 = "";
+        let line5 = "";
+        let randomValue = undefined;
+        const a = [true, false];
+        if (e.includes("npm")) {
+          if (e.includes("npm list")) {
+            line1 = `msa: what do you mean ${value}?\n`;
+            line2 = "";
+            line3 = "";
+            line4 = "";
+            line5 = ``;
+          } else if (e.includes("npm not")) {
+            line1 = `msa: package ${value} not registered\n`;
+            line2 = "";
+            line3 = "";
+            line4 = "";
+            line5 = ``;
+          } else {
+            randomValue = a[Math.floor(a.length * Math.random())];
+            line1 = "npm install MSA\n";
+            line2 = "Installing components...\n";
+            line3 = "Fetching from source...\n";
+            line4 = "Success 200 🤪\n";
+            line5 = `Failed 403 😭\n`;
+          }
+        }
+        if (e === "name") {
+          line1 = "My Name is Muh Syahendra A";
+          line2 = "";
+          line3 = "";
+          line4 = "";
+          line5 = ``;
+        }
+        if (e === "mount") {
+          line1 = "Instruction:\n";
+          line2 =
+            "\t\t npm install msa => for get my profile\n \t\tname or my name => for get my name\n \t\tclear => for clear text";
+          line3 = "";
+          line4 = "";
+          line5 = ``;
+        }
+        const birtday = new Date("1996-12-22");
+        const today = new Date();
+
+        const ageMilisecond = Number(today) - Number(birtday);
+        const ageYears = Math.floor(
+          ageMilisecond / (1000 * 60 * 60 * 24 * 365.25)
+        );
+
+        if (e === "age") {
+          line1 = ageYears + " " + "years old";
+          line2 = "";
+          line3 = "";
+          line4 = "";
+          line5 = ``;
+        }
+        if (e === "not found") {
+          line1 = "msa: command not found";
+          line2 = "";
+          line3 = "";
+          line4 = "";
+          line5 = ``;
+        }
+
+        if (line1) {
+          typed.type(line1, {
+            errorMultiplier: 0,
+            className:
+              ((e.includes("npm not") || e.includes("mount")) &&
+                "text-sm italic") ||
+              "",
+          });
+          typed.wait(1500);
+          if (container_typing)
+            container_typing.scrollTo({
+              left: 0,
+              top: 10000000000000000,
+              behavior: "smooth",
+            });
+        }
+        if (line2) {
+          typed.type(line2, {
+            errorMultiplier: 0,
+            className: (e.includes("mount") && "text-sm italic") || "",
+          });
+          typed.wait(1500);
+          if (container_typing)
+            container_typing.scrollTo({
+              left: 0,
+              top: 10000000000000000,
+              behavior: "smooth",
+            });
+        }
+        if (line3) {
+          typed.type(line3, { errorMultiplier: 0 });
+          typed.wait(1500);
+          if (container_typing)
+            container_typing.scrollTo({
+              left: 0,
+              top: 10000000000000000,
+              behavior: "smooth",
+            });
+        }
+        if (randomValue) {
+          if (line4) {
+            typed.type(line4, {
+              errorMultiplier: 0,
+              className: "successTyped",
+            });
+            typed.wait(1500);
+            if (container_typing)
+              container_typing.scrollTo({
+                left: 0,
+                top: 10000000000000000,
+                behavior: "smooth",
+              });
+
+            typed.type("====================\n", { errorMultiplier: 0 });
+            typed.wait(1000);
+            if (container_typing)
+              container_typing.scrollTo({
+                left: 0,
+                top: 10000000000000000,
+                behavior: "smooth",
+              });
+
+            typed.type("MSA Begin\n", {
+              errorMultiplier: 0,
+              className: "fontWeight",
+            });
+            typed.wait(1000);
+            typed.type("====================\n", { errorMultiplier: 0 });
+            if (container_typing)
+              container_typing.scrollTo({
+                left: 0,
+                top: 10000000000000000,
+                behavior: "smooth",
+              });
+
+            typed.wait(1000);
+            if (container_typing)
+              container_typing.scrollTo({
+                left: 0,
+                top: 10000000000000000,
+                behavior: "smooth",
+              });
+          }
+        } else {
+          if (line5) {
+            typed.type(line5, {
+              errorMultiplier: 0,
+              noSpecialCharErrors: true,
+              className: "errorTyped",
+            });
+            typed.wait(3000);
+            if (container_typing)
+              container_typing.scrollTo({
+                left: 0,
+                top: 10000000000000000,
+                behavior: "smooth",
+              });
+
+            typed.type("====================\n", { errorMultiplier: 0 });
+            typed.wait(1000);
+            if (container_typing)
+              container_typing.scrollTo({
+                left: 0,
+                top: 10000000000000000,
+                behavior: "smooth",
+              });
+
+            typed.type("Oh no, please try again...\n", {
+              errorMultiplier: 0,
+              className: "fontWeight",
+            });
+            typed.wait(1000);
+            if (container_typing)
+              container_typing.scrollTo({
+                left: 0,
+                top: 10000000000000000,
+                behavior: "smooth",
+              });
+
+            typed.type("====================\n", { errorMultiplier: 0 });
+            typed.wait(1000);
+          }
+        }
+        await typed.run();
+        // console.log(
+        //   typing_text,
+        //   dataTerminal.length,
+        //   document.getElementById(`typing_text` + dataTerminal.length),
+        //   "inityping_text"
+        // );
+        setDataTerminal((prev) => [
+          ...prev,
+          {
+            html: "input",
+          },
+        ]);
+
+        if (container_typing)
+          container_typing.scrollTo({
+            left: 0,
+            top: 10000000000000000,
+            behavior: "smooth",
+          });
+
+        setCodeStatus(randomValue);
+      };
+      type();
+    };
+    typeTexts("mount");
     sessionStorage.removeItem("poor");
   }, []);
 
@@ -287,8 +512,17 @@ function App() {
     if (charCode === 13 || e.key === "Enter") {
       typing_text.blur();
       typing_text.setAttribute("contentEditable", "false");
+      if (!value) {
+        setDataTerminal([
+          ...dataTerminal,
+          {
+            html: "text",
+          },
+        ]);
+        return;
+      }
 
-      if (value?.includes("npm")) {
+      if (value.includes("npm")) {
         if (value.toLocaleLowerCase().replace(/^\s{1,}/gm, "") === "npm") {
           setDataTerminal([
             ...dataTerminal,
@@ -336,7 +570,7 @@ function App() {
       }
       if (
         value
-          ?.toLocaleLowerCase()
+          .toLocaleLowerCase()
           .replace(/^\s{1,}/gm, "")
           .includes("name") ||
         value
@@ -355,11 +589,11 @@ function App() {
       }
       if (
         value
-          ?.toLocaleLowerCase()
+          .toLocaleLowerCase()
           .replace(/^\s{1,}/gm, "")
           .includes("umur") ||
         value
-          ?.toLocaleLowerCase()
+          .toLocaleLowerCase()
           .replace(/^\s{1,}/gm, "")
           .includes("age")
       ) {
@@ -372,7 +606,12 @@ function App() {
         typeText("age");
         return;
       }
-      if (value?.toLocaleLowerCase().replace(/^\s{1,}/gm, "") === "clear") {
+      if (
+        value
+          .toLocaleLowerCase()
+          .replace(/^\s{1,}/gm, "")
+          .includes("clear")
+      ) {
         setDataTerminal([
           {
             html: "input",
@@ -384,7 +623,7 @@ function App() {
         typeof value?.toLocaleLowerCase().replace(/^\s{1,}/gm, ""),
         "inifalse"
       );
-      if (value?.toLocaleLowerCase().replace(/^\s{1,}/gm, "") === "") {
+      if (value.toLocaleLowerCase().replace(/^\s{1,}/gm, "") === "") {
         setDataTerminal([
           ...dataTerminal,
           {
